136df6c88d1011f77743639a86452eda9852eb47
minoeru <minoeru@users.noreply.github.com>
2020-03-15 16:24:07 +0900

    add day 7

---
+++ 07day/harib04b/bootpack.c
@@ -0,0 +1,42 @@
+#include "bootpack.h"
+
+extern struct  KEYBUF keybuf;
+
+void HariMain(void){
+  
+  struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+  char s[40], mcursor[256];
+  int mx,my,i;
+  
+  init_gdtidt();
+  init_pic();
+  io_sti(); //CPUの割り込み禁止を解除
+
+  io_out8(PIC0_IMR, 0xf9); //PIC1とキーバードを許可
+  io_out8(PIC1_IMR, 0xef); //マウスを許可
+  
+  init_palette();
+  init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+  mx = (binfo->scrnx - 16) / 2;
+  my = (binfo->scrny - 28 - 16) / 2;
+  init_mouse_cursor8(mcursor, COL8_008484);
+  putblock8_8(binfo->vram,binfo->scrnx,16,16,mx,my,mcursor,16);
+  sprintf(s,"(%d,%d)",mx,my);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+  
+  for(;;){
+    io_cli();
+    if (keybuf.flag == 0){
+      io_stihlt();
+    }
+    else{
+      i = keybuf.data;
+      keybuf.flag = 0;
+      io_sti();
+      sprintf(s, "%x", i);
+      boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+      putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+    }
+  }
+}
+
