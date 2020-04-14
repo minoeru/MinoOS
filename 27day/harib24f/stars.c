ec671ab2c37d490456138d40dafc6ab2d054b868
minoeru <minoeru@users.noreply.github.com>
2020-04-14 17:10:06 +0900

    add day 27 //remove noodle.c

---
+++ 27day/harib24f/stars.c
@@ -0,0 +1,23 @@
+#include "apilib.h"
+
+unsigned long rand (void){
+  static unsigned long rand;
+  rand *= 162143;
+  rand += 39703;
+  return rand;
+}
+
+void HariMain(void){
+	char *buf;
+	int win, i, x, y;
+	api_initmalloc();
+	buf = api_malloc(150 * 100);
+	win = api_openwin(buf, 150, 100, -1, "stars");
+	api_boxfilwin(win,  6, 26, 143, 93, 0);
+	for (i = 0; i < 50; i++) {
+		x = (rand() % 137) +  6;
+		y = (rand() %  67) + 26;
+		api_point(win, x, y, 3);
+	}
+	api_end();
+}
